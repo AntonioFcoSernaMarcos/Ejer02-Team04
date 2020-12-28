5f5bbe1f1194767f329306552ebe1ff0db96746e
Antonio Francisco Serna Marcos <antoniofco09@gmail.com>
2020-12-28 19:03:05 +0100

    pideAño

--- src/com/ejer/LuckyNumber.java
+++ src/com/ejer/LuckyNumber.java
@@ -35,6 +35,35 @@ public class LuckyNumber {
       return mes;  
    
     }
+    public static int PideAño(){
+        int año=0;
+        do{
+         try{
+             System.out.print("Digame el año de su nacimiento(introduzca 0 para salir): ");
+             año=entrada.nextInt();
+             leido=true;
+             
+            if (año<0){
+                System.out.println("No existen años negativos a no ser que seas un niandertal");
+                leido=false;
+                entrada.nextLine();
+                
+            }
+            else if (año>2020){
+                System.out.println("Este numero no es válido debido a que sino serias un espermatozoide");
+                leido=false;
+                entrada.nextLine();
+            }
+             
+         }   catch(InputMismatchException ex){
+             System.out.println("Este valor no es numero entero por favor introduzca otro valor");
+             leido=false;
+             entrada.nextLine();
+         }
+        }while(leido==false);
+        
+        return año;
+    }
 	public static void main(String[] args) {
 
 		
